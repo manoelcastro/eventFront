5297c273232e8091a2ece037adf290bad5e04557
Manoel Castro <manoelcastrodev@gmail.com>
2023-01-08 17:01:24 -0300

    feat: Fetch funcionando

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,31 @@
 import type { Actions } from './$types';
  
 export const actions: Actions = {
-  default: async (event) => {
-    console.log(event)
+  default: async ({ request }) => {
+    const formData = await request.formData()
+    const name = formData.get('name')
+    const email = formData.get('email')
+    const fone = formData.get('fone')
+    const isSocial = formData.get('isSocial')
+    const social = formData.get('social')
+    const knowPayment = formData.get('knowPayment')
+
+    const data = {
+      name,
+      email,
+      fone,
+      isSocial: isSocial === 'on'? true: false,
+      social,
+      knowPayment: knowPayment === 'on'? true: false
+    }
+
+    const response = await fetch('http://localhost:1337/api/participants', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({data})
+    })
+    console.log(response)
   }
 };
